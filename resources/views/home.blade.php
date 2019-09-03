1644ddc6109d1040e1b6a2f6738cd904bf9149fd
checodelo <mogmarino@hotmail.com>
2019-09-02 22:44:38 -0300

    layout-home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,151 @@
-@extends('layouts.app')
+@extends('layout')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('titulo')
+  Home
+@endsection
+@section('css')
+  <link rel="stylesheet" href="/css/home.css">
+@endsection
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+@section('main')
+  <div id="carouselExampleControls" class="carousel slide mt-3 mb-3" data-ride="carousel">
+  <div class="carousel-inner ">
+    <div class="carousel-item active ">
+      <img src="/img/productos.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+          <button class="btn btn-primary btn-lg">Productos</button>
+      </div>
     </div>
+    <div class="carousel-item">
+      <img src="/img/gato.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block text-secondary">
+
+          <button class="btn btn-primary btn-lg">Categorias</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/img/marcas.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+
+          <button class="btn btn-primary btn-lg">Marcas</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/img/categorias.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+
+          <button class="btn btn-primary btn-lg ">Contacto</button>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="row bg-primary text-white mt-3 mb-3">
+  <h3 class="col-12 mt-3 mb-3">
+    <i class="far fa-comment-dots ml-3"></i>
+    <span class="ml-1">Preguntas Frecuentes FAQ</span>
+  </h3>
+  <div class="col-4 mt-3 mb-3">
+    <h6 class="d-flex">
+      <strong>
+        ¿Por que mi pajaro se llama Claudio Paul?
+      </strong>
+    </h6>
+    <p class="d-flex">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+
+  </div>
+  <div class="col-4 mt-3 mb-3">
+    <h6 class="d-flex">
+      <strong>
+        ¿Por que mi gato Sessa es violento?
+      </strong>
+    </h6>
+    <p class="d-flex">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+
+  </div>
+  <div class="col-4 mt-3 mb-3">
+    <h6 class="d-flex">
+      <strong>
+        ¿Por que Higuita hace el alacran?
+      </strong>
+    </h6>
+    <p class="d-flex">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </div>
+
+</div>
+
+<div class="row bg-primary">
+  <div class="col-4 text-white mt-4">
+    <h3>Nuestra misión</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </div>
+  <div class="col-4 text-white mt-4">
+    <h3>Contacto</h3>
+    <p>
+      <i class="fas fa-home mr-3"></i>
+      Mendoza, Argentina
+    </p>
+    <p>
+      <i class="fas fa-envelope mr-3"></i>
+      info@petshop.com
+    </p>
+    <p>
+      <i class="fas fa-phone mr-3"></i>
+      +54 9261 346-6706
+    </p>
+  </div>
+  <div class="col-4 text-white mt-4">
+    <h3 class="ml-4">Redes</h3>
+    <ul>
+      <li>
+        <a href="https://www.facebook.com/digitalhouse.edu/">
+          <i class="fab fa-facebook-f text-white"></i>
+          <span class="text-white ml-2">Facebook</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/_digitalhouse?lang=es">
+          <i class="fab fa-twitter text-white"></i>
+          <span class="text-white ml-2">Twitter</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com/channel/UCKkPOtW8gQPgIUaxF4Og7PA/videos">
+          <i class="fab fa-youtube text-white"></i>
+          <span class="text-white ml-2">YouTube</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/school/digital-house-argentina/">
+          <i class="fab fa-linkedin-in text-white"></i>
+          <span class="text-white ml-2">LinkedIn</span>
+        </a>
+      </li>
+    </ul>
+
+  </div>
+
+</div>
+<div class="row bg-primary justify-content-center text-white mt-3 mb-3">
+  <h3 class="mt-3 mb-3">
+    <i class="fas fa-map-marked-alt"></i>
+    Ubicación
+  </h3>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.2236972927058!2d-68.85965465970467!3d-32.89225336631603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e09e08865d9f7%3A0x6a62c1f49ef33549!2sDigital%20House%20-%20Mendoza!5e0!3m2!1ses!2sar!4v1567465660633!5m2!1ses!2sar" width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 </div>
 @endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
+    @yield('css')
+    @yield('js')
+
+    <title>
+      @yield('titulo')
+    </title>
+  </head>
+  <body>
+    <div class="container">
+      <header class="row bg-primary ">
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary ">
+          <a class="navbar-brand text-white" href="/">Pet-Shop</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">
+              Productos
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">
+              Categorias
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">
+              Marcas
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Usuarios
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Iniciar Sesión</a>
+              <a class="dropdown-item" href="#">Registrarse</a>
+            </div>
+          </li>
+        </ul>
+          </div>
+        </nav>
+      </header>
+      <main>
+        @yield('main')
+      </main>
+      <footer>
+
+        <div class="row bg-primary justify-content-center text-white">
+          <h6 class="mt-3">
+            <i class="far fa-copyright"></i>
+            2019 - PET SHOP - <strong>Todos los derechos reservados.</strong>
+          </h6>
+        </div>
+
+      </footer>
+
+    </div>
+    <script src="https://kit.fontawesome.com/76186c1f83.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
